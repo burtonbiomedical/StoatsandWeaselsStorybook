a68cd3a672f0f14c4d30de68dc7bb6b1a41020f6
Burton Biomedical <ross@burtonbiomedical.com>
2018-01-26 23:40:24 +0000

    added config and keys

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+//Imports
 const express = require('express');
 const mongoose = require('mongoose');
-
+//App declaration
 const app = express();
 
+//ROUTES
 app.get('/', (req, res) => {
   res.send("IT WORKS");
 });
 
+//PORT
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
