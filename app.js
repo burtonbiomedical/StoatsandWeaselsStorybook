9c299be4bd193aea7ca68b6c17c2bd8ac7682a87
Burton Biomedical <ross@burtonbiomedical.com>
2018-01-27 15:42:35 +0000

    added google oauth

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 //Imports
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 //App declaration
 const app = express();
 
+//Passport Config
+require('./config/passport')(passport);
+
 //ROUTES
+const auth = require('./routes/auth');
+//index route
 app.get('/', (req, res) => {
   res.send("IT WORKS");
 });
+//Use routes
+app.use('/auth', auth);
 
 //PORT
 const port = process.env.PORT || 5000;
