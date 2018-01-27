0efb2e4239537f30af2c49372bc9bc98170e9a7f
Burton Biomedical <ross@burtonbiomedical.com>
2018-01-27 22:27:17 +0000

    Created views and routes, began building UX

--- app.js
+++ app.js
@@ -2,20 +2,65 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
-//App declaration
-const app = express();
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+//Load keys
+const keys = require('./config/keys')
+
+//Map global promises
+mongoose.Promise = global.Promise;
+//Mongoose connect
+mongoose.connect(keys.mongoURI,{
+  useMongoClient: true
+}).then(() => {
+  console.log("Mongodb connected...")})
+  .catch(err => {
+  console.log(err)
+  });
+
+//Load User Model
+require('./models/Users');
 
 //Passport Config
 require('./config/passport')(passport);
 
+//Initialise App
+const app = express();
+
+//Handlebars middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+
+//Session and cookie-parser middleware
+app.use(cookieParser());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Set global vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+});
+
 //ROUTES
 const auth = require('./routes/auth');
-//index route
-app.get('/', (req, res) => {
-  res.send("IT WORKS");
-});
+const index = require('./routes/index');
+
 //Use routes
 app.use('/auth', auth);
+app.use('/', index)
 
 //PORT
 const port = process.env.PORT || 5000;
