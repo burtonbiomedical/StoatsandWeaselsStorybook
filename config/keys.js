7ad391004a38b37a52254841fbce7e678f531c04
Burton Biomedical <ross@burtonbiomedical.com>
2018-01-28 20:06:52 +0000

    final build

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:'mongodb://admin:admin91255@ds117158.mlab.com:17158/storybooks-dev',
-  googleClientID:'594642911418-cng6puogpt0i2vkl5n9k06k68c6fdav3.apps.googleusercontent.com',
-  googleClientSecret:'vQymfXIezRtgxYkEkcrJgsSo'
+if(process.env.NODE_ENV === 'production'){
+  module.exports = require('./keys_prod');
+}else{
+  module.exports = require('./keys_dev');
 };
